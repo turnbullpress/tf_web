6bbc68bd45aae3655f62dc0138ca96e1ac63d6f5
James Turnbull <james@lovedthanlost.net>
2016-10-28 05:24:41 -0400

    Added interface.tf

---
+++ interface.tf
@@ -0,0 +1,53 @@
+variable "region" {
+  description = "The AWS region."
+}
+
+variable "environment" {
+  description = "The name of our environment, i.e. development."
+}
+
+variable "key_name" {
+  description = "The AWS key pair to use for resources."
+}
+
+variable "public_subnet_ids" {
+  default     = []
+  description = "The list of public subnets to populate."
+}
+
+variable "private_subnet_ids" {
+  default     = []
+  description = "The list of private subnets to populate."
+}
+
+variable "ami" {
+  default = {
+    "us-east-1" = "ami-f652979b"
+    "us-west-1" = "ami-7c4b331c"
+  }
+
+  description = "The AMIs to use for web and app instances."
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "The instance type to launch "
+}
+
+variable "web_instance_count" {
+  default = 1
+  description = "The number of Web instances to create"
+}
+
+variable "app_instance_count" {
+  default = 1
+  description = "The number of App instances to create"
+}
+
+variable "vpc_id" {
+  description = "The VPC ID to launch in"
+}
+
+variable "domain" {
+  description = "The domain to use for the web service"
+}
