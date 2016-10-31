13b6345ea842465b1425efac33b26a0f1c54b402
James Turnbull <james@lovedthanlost.net>
2016-10-31 06:00:10 -0400

    Moved outputs to interface.tf

--- interface.tf
+++ interface.tf
@@ -51,3 +51,15 @@ variable "vpc_id" {
 variable "domain" {
   description = "The domain to use for the web service"
 }
+
+output "web_elb_address" {
+  value = "${aws_elb.web.dns_name}"
+}
+
+output "web_host_addresses" {
+  value = ["${aws_instance.web.*.private_ip}"]
+}
+
+output "app_host_addresses" {
+  value = ["${aws_instance.app.*.private_ip}"]
+}
